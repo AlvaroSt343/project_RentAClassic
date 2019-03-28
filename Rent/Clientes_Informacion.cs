08fd7b1a5fe9571e688e12fe3fd9c15d30f3117f
Alvaro S <red.fear@hotmail.com>
2019-03-28 02:37:10 -0600

    cambios de diseño info clientes

--- Rent/Clientes_Informacion.cs
+++ Rent/Clientes_Informacion.cs
@@ -17,137 +17,13 @@ namespace Rent
         public Clientes_Informacion()
         {
             InitializeComponent();
-
-            
-                ConsultaCliente();
-            
+            ConsultaCliente();
         }
         bool estado = false;
         
         string l,n;
 
-        private void Clientes_Alta_Load(object sender, EventArgs e)
-        {
 
-        }
-
-
-        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-
-        }
-
-        private void toolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void toolStripSplitButton1_ButtonClick(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox5_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint_1(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void textBox7_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label7_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox6_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void textBox4_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
         private void ConsultaCliente()
         {
             if (estado == false)
@@ -192,17 +68,12 @@ namespace Rent
                 }
             }
         }
-        private void label5_Click_1(object sender, EventArgs e)
-        {
-
-        }
 
         public void RecibeCliente(string n) {
             estado = true;
             this.n = n;
             ConsultaCliente();
             ConsultaRentas();
-            
 
         }
 
@@ -219,7 +90,6 @@ namespace Rent
             Folio.DataSource = tht.Tables["Listado"].DefaultView;
             verinfo.DataSource = null;
             verinfo.Refresh();
-
 
             return 0;
         }
@@ -256,11 +126,6 @@ namespace Rent
             return 0;
         }
 
-        private void Folio_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
         private void Folio_MouseClick(object sender, MouseEventArgs e)
         {
             l = Folio.SelectedRows[0].Cells[0].Value.ToString();
@@ -272,9 +137,6 @@ namespace Rent
             this.Close();
         }
 
-        private void verinfo_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
 
-        }
     }
 }
